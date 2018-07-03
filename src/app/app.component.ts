2037fa161d4af5b24b15188021986711ebee0e73
Chris Schwartz <chris.f.schwartz@gmail.com>
2018-07-02 21:44:44 -0500

    Wires guesses from LetterGuessComponent to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GuessesComponent } from './guesses/guesses.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  puzzle = "Neat"
+
+  incorrectGuesses = [];
+  correctGuesses = [];
+
+
+  onGuessMade(newGuess : string) {
+    if(this.correctGuesses.includes(newGuess) || this.incorrectGuesses.includes(newGuess)) {
+        // do nothing, guess was already made...
+        return
+    }
+
+    if(this.puzzle.includes(newGuess)) {
+      this.correctGuesses.push(newGuess)
+    } else {
+      this.incorrectGuesses.push(newGuess)
+    }
+  }
+
 }
