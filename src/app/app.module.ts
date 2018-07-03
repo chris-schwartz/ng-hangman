5748f59f50ba214343dccd2221f4c0349a3f02bb
Chris Schwartz <chris.f.schwartz@gmail.com>
2018-07-02 19:43:20 -0500

    Adds hangman image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HangmanComponent } from './hangman/hangman.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HangmanComponent
   ],
   imports: [
     BrowserModule
