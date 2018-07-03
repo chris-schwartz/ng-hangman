7a1833fa2c303e112a1342e9fde5fa23802253c7
Chris Schwartz <chris.f.schwartz@gmail.com>
2018-07-02 21:25:00 -0500

    Adds behavior to LetterGuessComponent to validate user input and emit the guess when the Guess button is pressed

--- src/app/letter-guess/letter-guess.component.ts
+++ src/app/letter-guess/letter-guess.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-letter-guess',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./letter-guess.component.css']
 })
 export class LetterGuessComponent implements OnInit {
+  currentGuess = ""
+  errorMessage = ""
+
+  @Output("guess") guessEmitter = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onGuessChange(event: any) {
+    const guess = event.target.value
+
+    if (/^([A-z]{1})$/.test(guess)) {
+      this.currentGuess = guess;
+      this.errorMessage = ""
+    } else if( guess === "") {
+      this.currentGuess = "";
+    } else {
+      this.errorMessage = "Must enter a single letter for the guess"
+    }
+  }
+
+  onGuessPressed() {
+    this.guessEmitter.emit(this.currentGuess)
+  }
+
 
 }
