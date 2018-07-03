ea1b5efd1cd9a93e489018b6d2bc7dd2eb137820
Chris Schwartz <chris.f.schwartz@gmail.com>
2018-07-02 19:50:51 -0500

    Adds puzzle component, centers image in hangman component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HangmanComponent } from './hangman/hangman.component';
+import { PuzzleComponent } from './puzzle/puzzle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HangmanComponent
+    HangmanComponent,
+    PuzzleComponent
   ],
   imports: [
     BrowserModule
